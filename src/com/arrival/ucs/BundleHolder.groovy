0218b06078e1e81103002ad155f82dbad04e0a3a
Ilia Lopatin <iv.lopatin@outlook.com>
2024-04-08 14:17:30 +0400

    update class

--- src/com/arrival/ucs/BundleHolder.groovy
+++ src/com/arrival/ucs/BundleHolder.groovy
@@ -4,37 +4,37 @@ import groovy.json.JsonSlurper
 
 
 class BundleHolder extends BundleHolderClazz {
-    String initBundles = """
-            {
-        "Van_1": [
-            {"//vehicle1": "0.0.9.0"},
-            {"//vehicle2": "0.0.9.0"}
-        ],
-        "Van_2": [
-            {"//vehicle1": "0.2.0.0"}
-        ],
-        "Van_3": [
-            {"//vehicle1": "0.13.0.2"},
-            {"//vehicle2": "0.5.0.1"},
-            {"//vehicle3": "0.6.0.1"}
-        ],
-        "Van_4": [
-            {"//vehicle1": "head"},
-            {"//vehicle2": "head"},
-            {"//vehicle3": "head"},
-            {"//vehicle4": "head"},
-            {"//vehicle5": "head"}
-        ],
-        "Van_5": [
-            {"//vehicle1": "0.22.0.5"},
-            {"//vehicle2": "0.22.6.0"}
-        ]
-        }
-    """
+    // String initBundles = """
+    //         {
+    //     "Van_1": [
+    //         {"//vehicle1": "0.0.9.0"},
+    //         {"//vehicle2": "0.0.9.0"}
+    //     ],
+    //     "Van_2": [
+    //         {"//vehicle1": "0.2.0.0"}
+    //     ],
+    //     "Van_3": [
+    //         {"//vehicle1": "0.13.0.2"},
+    //         {"//vehicle2": "0.5.0.1"},
+    //         {"//vehicle3": "0.6.0.1"}
+    //     ],
+    //     "Van_4": [
+    //         {"//vehicle1": "head"},
+    //         {"//vehicle2": "head"},
+    //         {"//vehicle3": "head"},
+    //         {"//vehicle4": "head"},
+    //         {"//vehicle5": "head"}
+    //     ],
+    //     "Van_5": [
+    //         {"//vehicle1": "0.22.0.5"},
+    //         {"//vehicle2": "0.22.6.0"}
+    //     ]
+    //     }
+    // """
 
     BundleHolder(def pipeline) {
         super(pipeline)
-        this.initializeFromString(this.initBundles)
+        // this.initializeFromString(this.initBundles)
 
     }
 }
