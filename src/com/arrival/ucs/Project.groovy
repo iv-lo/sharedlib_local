c66db8ca4f862e7e96ce746ec19f8390f4bcbd53
Ilia Lopatin <iv.lopatin@outlook.com>
2024-04-09 18:13:15 +0400

    update class

--- src/com/arrival/ucs/Project.groovy
+++ src/com/arrival/ucs/Project.groovy
@@ -2,7 +2,10 @@ package com.arrival.ucs
 
 
 class Project extends ProjectClazz {
-    Project(def pipeline) {
+    Project(def pipeline, String project, String version, String commit) {
         super(pipeline)
+        this.project = project
+        this.version = version
+        this.commit = commit
     }
 }
